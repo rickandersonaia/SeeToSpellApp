4e6b8b7d18419607d12f6204e9c02973287b7d56
Rick Anderson <rick@byobwebsite.com>
2017-12-24 17:22:15 -0800

    Initial Heroku Deployment

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,4 +1,4 @@
-export const baseURL = 'http://localhost:4300/';
+export const baseURL = 'https://seetospellapp.herokuapp.com/';
 export const imageURL = baseURL + 'images/';
 export const audioURL = baseURL + 'audio/';
 export const avatarURL = baseURL + 'avatars/';
